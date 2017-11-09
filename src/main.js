4fa35c2396b310199578911d8c9d27c9f290be04
Hayden <meloche.hayden@gmail.com>
2017-11-08 20:38:59 -0500

    Bot updates

--- src/main.js
+++ src/main.js
@@ -18,6 +18,7 @@ var botPrefix = '+';
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setGame('developed by @root');
+    writeLeaderboards('test message sent on startup2', 10, '136607366408962048');
 });
 
 client.on('guildCreate', newGuild => {
@@ -42,12 +43,42 @@ client.on('guildCreate', newGuild => {
         });
 });
 
+/**
+ * Event that happens when reaction is removed
+ * If reaction is a star then -1 from its score. If for some reason its not in the database then add it.
+ */
+client.on('messageReactionRemove', reaction => {
+    if (reaction.emoji.name === '⭐') {
+        const message = reaction.message;
+        sql.get(`SELECT score FROM "${reaction.message.guild.id}" WHERE messageid ="${reaction.message.id}"`).then(row => {
+        if (!row) {
+            sql.run(`INSERT INTO "${reaction.message.guild.id}" (author, message, score, messageid) VALUES (?, ?, ?, ?)`, [message.author.id, message.content, reaction.count.toPrecision, message.id])
+            .catch(() => {
+                console.error;
+            });
+        } else {
+            sql.run(`Update "${reaction.message.guild.id}" SET score = score - 1 where messageid = "${message.id}"`)
+            .catch(() => {
+                console.error;
+            });
+        }
+        }).catch(() => {
+            console.error;
+        });
+    }
+
+});
+
+/**
+ * Event that happens on reaction being added
+ * If reaction is a star then add it to the database / update its score if its already there
+ */
 client.on('messageReactionAdd', reaction => {
     if (reaction.emoji.name === '⭐') {
         const message = reaction.message;
         sql.get(`SELECT score FROM "${reaction.message.guild.id}" WHERE messageid ="${reaction.message.id}"`).then(row => {
         if (!row) {
-            sql.run(`INSERT INTO "${reaction.message.guild.id}" (author, message, score, messageid) VALUES (?, ?, ?, ?)`, [message.author.id, message.content, 1, message.id])
+            sql.run(`INSERT INTO "${reaction.message.guild.id}" (author, message, score, messageid) VALUES (?, ?, ?, ?)`, [message.author.id, message.content, reaction.count.toPrecision, message.id])
             .catch(() => {
                 console.error;
             });
@@ -63,6 +94,9 @@ client.on('messageReactionAdd', reaction => {
     }
 }); 
 
+/**
+ * Event handler for when a message is recieved.
+ */
 client.on('message', message => {
 
     //Ignore bots so we don't have botception
@@ -82,13 +116,20 @@ client.on('message', message => {
     
 });
 
-function writeLeaderboards() {
-    const starboard = reaction.message.guild.channels.find('name', 'hall-of-fame');
+/**
+ * Takes a message and a user and prints an embed
+ * @param {string} message 
+ * @param {number} stars 
+ * @param {number} userID 
+ */
+function writeLeaderboards(message, stars, userID) {
+    const starboard = client.guilds.find('id','247186623782060042').channels.find('name', 'starboard');
+    var user = client.users.get('id') //needs to be fixed
     var embed = new Discord.RichEmbed()
-        .setAuthor(`Hall of fame nomination with ${starCount + (starCount > 1 ? ' stars' : ' star' )}`)
-        .addField(`Author:`, reaction.message.author.username)
-        .addField('Message:', reaction.message.content)
-        .setImage(reaction.message.author.avatarURL)
+        .setAuthor(`Hall of fame nomination with ${stars + (stars > 1 ? ' stars' : ' star' )}`)
+        .addField(`Author:`, user.tag)
+        .addField('Message:', message)
+        .setImage(user.avatarURL)
         .setFooter('Upvote this message using the ⭐ emoji!')
     starboard.send({embed});
 }
